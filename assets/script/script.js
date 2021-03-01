2d9243023302db7849b369fdcb6e33b932e825cc
Maria M <prov21_1@hotmail.com>
2021-02-28 21:22:23 -0700

    save to storage

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,7 +1,10 @@
 // Create score variable
 var score = 0;
+//document.querySelector('#score').addEventListener('click', loadScores);
+
 //display it in window
-document.querySelector('#score').textContent =  "Score: " + score;
+//document.querySelector('#score').textContent =  "Score: " + score;
+
 
 
 // save it in localStorage
@@ -12,23 +15,80 @@ var centerDiv = document.querySelector('#centerDiv');
 
 var questionNumber = 0;
 
+var saveScore = function(initials) {
+    // save initials and score
+    // make an array for high scores but if there are none, make an array
+    // name items pulled from local storage "highscores"
+    if (!localStorage.getItem("highscores")) {
+    var highscores =[];
+    }
+    else {var highscores = localStorage.getItem("highscores");
+    }   
+    // make an object to hold scores
+    var scoreToAdd = {
+        initials: initials,
+        score: score,
+    };
+    //add object to high scores array
+    //highscores.push(scoreToAdd);
+    localStorage.setItem("highscores", JSON.stringify(
+        highscores));
+        loadScores();
+    }
+
+    var clearScores = function() {
+        //clear scores from screen
+        //centerDiv.textContent="";
+        localStorage.clear;
+    }
+
+// display high scores
+var loadScores = function() {
+    //get highscores
+    var highscores=JSON.parse(localStorage.getItem("highscores"));
+    //put highscores on page
+
+    console.log(highscores);
+    // eliminate timer display and display high scores link
+    document.querySelector('#score').textContent="";
+    timer.innerText="";
+    // display "high scores"
+    centerDiv.textContent="High Scores";
+    // add clear and go back buttons
+    var clearBtn = document.createElement("BUTTON");
+    clearBtn.classList.add("btn-primary");
+    clearBtn.classList.add("btn");
+    //neither the query Selector nor the buttonDiv reference are working so these buttons are not appending
+    document.querySelector('#buttons').appendChild(clearBtn);
+    timeLeft = 0;
+    clearBtn.innerHTML="Clear scores";
+    var goBackBtn = document.createElement("BUTTON");
+    goBackBtn.classList.add("btn-primary");
+    goBackBtn.classList.add("btn");
+    buttonDiv.appendChild(goBackBtn);
+    goBackBtn.innerHTML="Go Back";
+    // use a callback function to pass an argument 
+    clearBtn.addEventListener('click', clearScores);
+    goBackBtn.addEventListener('click', startQuiz);
+}
+
 var quiz = [
     {
-        q: "this is question 1",
-        choices: ["a","b","c","d"],
-        correct: "a"},
+        q: "How old is JavaScript?",
+        choices: ["5 years","18 years","25 years","30 years"],
+        correct: "25 years"},
     {
-        q: "this is question 2",
-        choices: ["e","f","g","h"],
-        correct: "h"},
+        q: "toLowerCase is an example of a JavaScript...",
+        choices: ["object","array","conditional", "method"],
+        correct: "method"},
     { 
-        q: "this is question 3",
-        choices: ["a","b","c","d"],
-        correct: "c"},
+        q: "What is the keyword that returns a value from a function?",
+        choices: ["break","equals","return","value"],
+        correct: "return"},
     {
-        q: "this is question 4",
-        choices: ["a","b","c","d"],
-        correct: "c"},
+        q: "Which of the following words is used to declare a variable?",
+        choices: ["variable","var","lets", "cons"],
+        correct: "var"},
 ]
 var timer = document.getElementById('time');
 var timeLeft=59;
@@ -44,12 +104,14 @@ var startButton = document.querySelector('#startButton');
 startButton.addEventListener('click', startQuiz);
 
 var check = function() {
-if (this.innerHTML===quiz[0].correct) {
+    console.log(this.innerHTML);
+    console.log(quiz[questionNumber].correct);
+if (this.innerHTML===quiz[questionNumber].correct) {
     var alert = document.createElement('p');
     centerDiv.appendChild(alert);
     alert.innerText=("Correct!");
     score+=1;
-    document.querySelector('#score').textContent = "Score: " + score;
+//    document.querySelector('#score').textContent = "Score: " + score;
 //   console.log(score);
 } else {
     var alert = document.createElement('p');
@@ -62,9 +124,13 @@ setTimeout(function () {
     //this is not removing all the buttons,just the one that was clicked
     buttonDiv.innerHTML="";
     alert.innerText="";
+    //go to the next question
+    questionNumber++;
+    console.log(questionNumber);
     nextQuestion();
   }, 1000)
 }
+
 
 function nextQuestion() {
     if (questionNumber < 4) {
@@ -82,7 +148,6 @@ function nextQuestion() {
     // event Listener
     btn.addEventListener('click', check);
     };
-    questionNumber++;
 }
     // advance to the next question unless it's the last question
     else {
@@ -93,6 +158,7 @@ function nextQuestion() {
         var initials = document.createElement("input");
         initials.type = "text";
         buttonDiv.appendChild(initials);
+        // set var initials to the value of what's put in the initialsEl
         // add save button
         var btn = document.createElement("BUTTON");
         btn.classList.add("btn-primary");
@@ -100,7 +166,10 @@ function nextQuestion() {
         buttonDiv.appendChild(btn);
         timeLeft = 0;
         btn.innerHTML="Save my score";
-        btn.addEventListener('click', saveScore);
+        // use a callback function to pass an argument 
+        btn.addEventListener('click', function() {
+            saveScore(initials.value);
+        });
     }
 }
 //}
@@ -113,7 +182,7 @@ display your score and save button
 */
 
 function startQuiz() {
-  // remove the start button
+
     timeLeft--;
     // Add timer
 var timeInterval = setInterval(function() {
@@ -133,6 +202,7 @@ var timeInterval = setInterval(function() {
         clearInterval(timeInterval);
     }
     }, 1000);
+      // remove the start button
     document.querySelector('#startButton').remove();
     nextQuestion();
 }
